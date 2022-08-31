abe015471f3e9ead333da21f74cadf8e637d1b03
Fernando Zanchetta <fernando.zanchetta@empiricus.com.br>
2022-08-31 14:37:51 -0300

    async data

--- pages/company.vue
+++ pages/company.vue
@@ -1,11 +1,22 @@
 <template>
-  <h1>Company Page</h1>
+  <div>
+    <h1>Company Page</h1>
+    <pre>
+      {{companies}}
+    </pre>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'CompanyPage'
+  name: 'CompanyPage',
+  async asyncData({ $axios }) {
+    const companies = await $axios.$get('https://jsonplaceholder.typicode.com/posts')
+    return {
+      companies
+    }
+  }
 })
 </script>
